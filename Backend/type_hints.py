22cc08b328d2f9b3996b0f2af5bbd5eba44c01fb
Saray <srodrigueztrapero@alumnos.ilerna.com>
2026-05-13 11:51:09 +0200

    Add examples for type hints, variables, operators, strings, lists, tuples, sets, dictionaries, conditionals, loops, functions, exceptions, and modules

---
+++ Backend/type_hints.py
@@ -0,0 +1,17 @@
+### type hints ###
+
+my_string_variable: str = "My String variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str = "My Typed String Variable"
+print(my_typed_variable)
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
+
--- basic/13_modules.py
+++ basic/13_modules.py
@@ -1,11 +1,11 @@
 ### modules ###
 
-import module
+import basic.module as module
 
 module.sum(5, 10, 15)
 module.printValue("Hello, World!")
 
-from module import sum, printValue
+from basic.module import sum, printValue
 sumValue = sum(20, 30, 40)
 printValue("Holi Python")
 
